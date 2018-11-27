617302d5a0efbcfe3a87329a32873de413020640
stephyswe <stephaniiehallberg@gmail.com>
2018-11-27 15:37:29 +0100

    Posts with similar tags

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -85,9 +86,17 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                   'comments': comments,
                   'new_comment': new_comment,
-                  'comment_form': comment_form})
+                  'comment_form': comment_form,
+                  'similar_posts': similar_posts})
