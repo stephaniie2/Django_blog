6e0e54f8edc1d675647292e1ae2c4cd2bbf2f02d
stephyswe <stephaniiehallberg@gmail.com>
2018-11-27 04:07:48 +0100

    using class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ app_name = 'blog'
 # URL patterns allow you to map URLs to views.
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
