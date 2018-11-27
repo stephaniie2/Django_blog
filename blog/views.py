3da6687a0399bed7b840ddb05ce0cb5fa296d830
stephyswe <stephaniiehallberg@gmail.com>
2018-11-27 15:28:15 +0100

    adding the tagging functionality

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
+from taggit.models import Tag
+
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
@@ -34,8 +36,14 @@ class PostListView(ListView):
     paginate_by = 1
     template_name = 'blog/post/list.html'
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 1) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -50,7 +58,8 @@ def post_list(request):
     #posts = Post.published.all()
     return render(request,
                   'blog/post/list.html',
-                  {'page': page, 'posts': posts})
+                  {'page': page, 'posts': posts, 'tag': tag})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
