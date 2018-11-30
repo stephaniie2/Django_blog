23b48cd85b9b1d7e7c9f91bc7a9e815b1fc68f46
stephyswe <stephaniiehallberg@gmail.com>
2018-11-30 10:58:03 +0100

    basic search engine with postgres, instead of sqlite

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 from taggit.models import Tag
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 def post_share(request, post_id):
     # Retrieve post by id
@@ -100,3 +101,20 @@ def post_detail(request, year, month, day, post):
                   'new_comment': new_comment,
                   'comment_form': comment_form,
                   'similar_posts': similar_posts})
+                  
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.objects.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
